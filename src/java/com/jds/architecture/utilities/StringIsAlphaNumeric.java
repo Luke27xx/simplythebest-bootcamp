fe541bae346a978815d459f41eb43e926136ef8d
Dmitrijs.Sadovskis <Dmitrijs.Sadovskis@3f29d60b-5d35-0410-bcc3-1970b16363d6>
2007-07-16 14:24:02 +0000

    Revised.

--- src/java/com/jds/architecture/utilities/StringIsAlphaNumeric.java
+++ src/java/com/jds/architecture/utilities/StringIsAlphaNumeric.java
@@ -36,10 +36,10 @@ public class StringIsAlphaNumeric implements ValidationStrategy{
 	public boolean validate(Object target) {
 	
 		String s = target.toString();
-		for (int i=0; i<s.length(); i++)
-				if (Character.isLetterOrDigit(s.charAt(i))){}
-				else
-		        return false;
+		
+		for (int i = 0; i < s.length(); i++)
+			if (!Character.isLetterOrDigit(s.charAt(i)))
+				return false;
 		return true;
 		
 	}
