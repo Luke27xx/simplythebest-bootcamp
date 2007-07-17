3d8e5d5d08996eac64ac65e0a6088968768a9f46
Amittere <Amittere@3f29d60b-5d35-0410-bcc3-1970b16363d6>
2007-07-17 09:11:26 +0000

    implemented validation

--- src/java/com/jds/apps/hr/skill/set/form/SkillForm.java
+++ src/java/com/jds/apps/hr/skill/set/form/SkillForm.java
@@ -44,10 +44,47 @@ public class SkillForm extends AbstractSkillForm {
 			HttpServletRequest request) {
 
 		ActionErrors errors = new ActionErrors();
-
-//      TODO Implement validation
-
-        return errors;
+	   
+		   // checking on empty string
+		   Validator stringIsEmpty   =  new Validator ( new StringIsEmpty() );
+	     
+		   // defining valid symbols in string
+	       Validator stringIsValidE = new Validator( new StringIsValid("_- .\n\r,/\\+#()") );
+	
+           String allowedCharactersD = "letters & numbers, underscores, dashes, spaces, dots ";
+           String allowedCharactersE = allowedCharactersD + ", line carriage, commas, slashes, pluses, pounds, and parenthesis";        
+           
+           // defining max lenth of fields
+           Validator stringLengthIsValidFifty       = new Validator( new StringLengthIsValid(50) );        
+           Validator stringLenghtIsValidHundred 	= new Validator(new StringLengthIsValid(100));
+           
+           
+    // defining errors 
+           // Skill fields if its empty , contains illegal characters 
+           // or more than max lenght
+           if( stringIsEmpty.validate( this.getSkill() ) ) {
+        	   errors.add("skill", new ActionError("field.null" ,"skill field is empty"));
+           }
+           else if( !stringIsValidE.validate(this.getSkill() )  ) {
+   			errors.add("skill", new ActionError("field.invalid.specialcharacter", "skill allowed characters are ", allowedCharactersD));
+           }
+           else if( !stringLengthIsValidFifty.validate( this.getSkill() ) ) {
+   			errors.add("skill", new ActionError("field.invalid.length", "skill max length is ", "50"));        	
+           }  
+           
+           // Description field if its empty , contains illegal characters 
+           // or more than max lenght
+           if( stringIsEmpty.validate( this.getDescription() ) ) {
+        	   errors.add("description", new ActionError("field.null" ,"description field is empty"));
+           }
+           else if( !stringIsValidE.validate(this.getDescription() )  ) {
+   			errors.add("description", new ActionError("field.invalid.specialcharacter", "description allowed characters are ", allowedCharactersD));
+           }
+           else if( !stringLenghtIsValidHundred.validate( this.getDescription() ) ) {
+   			errors.add("description", new ActionError("field.invalid.length", "description max length is ", "100"));        	
+           }  
+    
+           return errors;
 	}
 	
 	public List getCategories() {
