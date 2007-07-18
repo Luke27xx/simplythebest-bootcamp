17334fe279f2122e59190c4594133bbd7b73a1ea
AngeSoleil <AngeSoleil@3f29d60b-5d35-0410-bcc3-1970b16363d6>
2007-07-18 08:09:51 +0000

    Lena

---
+++ src/java/com/jds/architecture/service/dao/assembler/ProjectAssembler.java
@@ -0,0 +1,57 @@
+package com.jds.architecture.service.dao.assembler;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+import javax.sql.RowSet;
+
+import com.jds.apps.beans.EmployeeInfo;
+import com.jds.apps.beans.ProjectInfo;
+import com.jds.architecture.service.dao.DAOConstants;
+
+public class ProjectAssembler {
+
+	/**
+	 * Populates prepared statement using details of value object
+	 * @param project Project value object
+	 * @param stmt prepared statement from data access object
+	 * @throws SQLException 
+	 */
+	public static void  getPreparedStatement(ProjectInfo project, 
+		PreparedStatement stmt) throws SQLException {
+		stmt.setString(1, project.getProjectId() );
+		stmt.setString(2, project.getProjectName() );
+		stmt.setString(3, project.getProjectDescrition() );
+		stmt.setDate(4, (Date) new java.util.Date(project.getStartDate().toString() ) );
+		stmt.setDate(5, (Date) new java.util.Date(project.getEndDate().toString() ) );
+	
+		
+			
+	}
+	
+	/**
+	 * Creates populated ProjectInfo vo from the Row set 
+	 * @param rs Row Set 
+	 * @return ProjectInfo
+	 * @throws SQLException
+	 */
+	public static ProjectInfo getInfo(ResultSet rs) throws SQLException   {
+		ProjectInfo projectReturn  = new ProjectInfo();
+		projectReturn.setClient(rs.getString("client") );
+		projectReturn.setProjectName(rs.getString("name") ); 
+		projectReturn.setProjectDescription(rs.getString("desc") );
+		projectReturn.setProjectId(rs.getString("id") );		
+		projectReturn.setStartDate(new java.util.Date(((java.sql.Timestamp)rs.getObject("startDate")).getTime()));
+		projectReturn.setEndDate(new java.util.Date(((java.sql.Timestamp)rs.getObject("endDate")).getTime()));
+		return projectReturn;
+	}
+	
+
+		public static void toEmptyStringAllNull(ProjectInfo obj) {
+		if(obj.getProjectDescrition() == null) obj.setProjectDescription(DAOConstants.STR_SPACE);
+		
+	}
+}
