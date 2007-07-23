a33bc736ca539bd57a105fba6edda105d77cb970
vitasius <vitasius@3f29d60b-5d35-0410-bcc3-1970b16363d6>
2007-07-23 07:16:48 +0000

    implemented

--- trunk/src/java/test/com/jds/architecture/service/dao/ProjectDAOTest.java
+++ trunk/src/java/test/com/jds/architecture/service/dao/ProjectDAOTest.java
@@ -1,9 +1,7 @@
-/**
- * 
- */
+
 package test.com.jds.architecture.service.dao;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -28,17 +26,43 @@ import com.jds.architecture.service.dao.DAOFactory;
 import com.jds.architecture.service.dao.DataAccessObjectInterface;
 import com.jds.architecture.service.dao.EmpAccentureDetailsDAO;
 import com.jds.architecture.service.dao.ProjectDAO;
+import com.mysql.jdbc.ResultSet;
+
+/**
+ * 
+ * @author Vytautas Streimikis
+ */
 
 public class ProjectDAOTest {
 	
 	private DataAccessObjectInterface dao;
 	private static Connector conn;
+	//private ProjectInfo project; 
+	
+	public ProjectInfo createObject(String cnt){
+		ProjectInfo item1 = new ProjectInfo();
+		item1.setProjectId(cnt);
+		item1.setProjectName("testproject"+cnt);
+		item1.setClient("testclient"+cnt);
+		item1.setDescription("testdescription"+cnt);
+		Calendar car = Calendar.getInstance();
+		car.set(2006,9,Integer.parseInt(cnt));
+		java.sql.Date date = new java.sql.Date(car.getTimeInMillis());
+		item1.setStartDate(date);
+		car.set(2006,10,Integer.parseInt(cnt));
+		date = new java.sql.Date(car.getTimeInMillis());
+		item1.setEndDate(date);
+		return item1;
+		
+	}
+	
 	
 	@BeforeClass
 	public static void onlyOnce() {
 		conn = new Connector("oracle.jdbc.driver.OracleDriver",
 				"jdbc:oracle:thin:@localhost:1521:XE", "hruser", "hruser");		
-		
+			
+	
 	}
 
 	
@@ -56,83 +80,100 @@ public class ProjectDAOTest {
 	}
 	
 	
-	/**
-	 * Deletes everything
-	 * @throws DAOException 
-	 * @throws DAOException 
-	 */
 	@After
 	public void tearDown() throws SQLException, DAOException {
 		conn.reconnect();
+		conn.clearAll();
 		conn.close();	
 	}
 
 	@Test
-	public void testCreate() throws DAOException {
+	public void testCreate() throws DAOException, SQLException {
 		
-		ProjectInfo item1 = new ProjectInfo();
-		item1.setProjectId("1");
-		item1.setProjectName("testproject");
-		item1.setClient("testclient");
-		item1.setDescription("testdescription");
-		
-		java.sql.Date date = new java.sql.Date(1000L*60*60*24);
-		item1.setStartDate(date);
-		item1.setEndDate(date);
+		ProjectInfo item1 = createObject("1");
 		dao.create(conn.getConnection(), item1);
+		Statement sql_stmt = conn.getConnection().createStatement();
+		java.sql.ResultSet rset = sql_stmt.executeQuery("SELECT id from project");
+		rset.next();
+		assertEquals("1", rset.getString("id"));
+		rset.close(); 
+		sql_stmt.close();
 				
 	}
 	
 	@Test
-	public void testfindbyPK() throws DAOException {
-		dao.findByPK("1");
-				
+	public void testfindbyPK() throws DAOException, SQLException {
+		
+		ProjectInfo item1 = createObject("1");
+		conn.createProject(1);
+		ProjectInfo item2= (ProjectInfo)dao.findByPK("1");
+		assertEquals(item1.getProjectId(), item2.getProjectId());
+		
+					
 	}
 	
 	@Test
 	public void testfind() throws DAOException, SQLException {
 		
-		ProjectInfo item1 = new ProjectInfo();
-		item1.setProjectId("1");
-		item1.setProjectName("testproject");
-		item1.setClient("testclient");
-		item1.setDescription("testdescription");
-		
-		item1.setStartDate(null);
-		item1.setEndDate(null);
+		ProjectInfo item1 = createObject("1");
+		conn.createProject(1);
 		RowSet rs = dao.find(item1);
 		int  i=0;
-		while (rs.next())
-		{
-			i++;
-		}
+		while (rs.next()){i++;}
 		assertEquals(1, i);
 				
 	}
 	
 	@Test
-	public void testupdate() throws DAOException {
-		ProjectInfo item1 = new ProjectInfo();
-		item1.setProjectName("testproject1");
-		item1.setClient("testclient1");
-		item1.setDescription("testdescription1");
+	public void testupdate() throws DAOException, SQLException {
 		
-		ProjectInfo item2 = new ProjectInfo();
-		item2.setProjectId("1");
-		
-		dao.update(conn.getConnection(),item1,item2);
+		ProjectInfo item1 = createObject("1");
+		ProjectInfo item2 = createObject("2");
+		conn.createProject(1);
+		dao.update(conn.getConnection(),item2,item1);
+		conn.reconnect();
+		Statement sql_stmt = conn.getConnection().createStatement();
+		java.sql.ResultSet rset = sql_stmt.executeQuery("SELECT id from project");
+		rset.next();
+		assertEquals("2", rset.getString("id"));
+		rset.close(); 
+		sql_stmt.close();
 				
 	}
 	
 	@Test
-	public void testremove() throws DAOException {
+	public void testremove() throws DAOException, SQLException {
+		
+		conn.createProject(1);
 		dao.remove(conn.getConnection(),"1");
+		conn.reconnect();
+		Statement sql_stmt = conn.getConnection().createStatement();
+		java.sql.ResultSet rset = sql_stmt.executeQuery("SELECT id from project");
+		if (rset.next()) fail("failed");
+		rset.close(); 
+		sql_stmt.close();
+				
+	}
+	
+	@Test
+	public void findbyall() throws DAOException, SQLException {
+		
+		conn.createProject(2);
+		RowSet rs = dao.findByAll();
+		int  i=0;
+		while (rs.next()){i++;}
+		assertEquals(2, i);
 				
 	}
 	
 	
 }
 
+/**
+ * 
+ * Connection class
+ *
+ */
 
 class Connector {
 	protected String dbDriver;
@@ -193,9 +234,31 @@ class Connector {
 	public void setDbUser(String dbUser) {
 		this.dbUser = dbUser;
 	}
-
+    
+	public void clearAll() {
+		try {
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate("DELETE FROM project");
+			stmt.close();
+		} catch (Exception ex) {
+		}
+	}
+	
+	public void createProject(int cnt) {
+		try {
+			Statement stmt = conn.createStatement();
+			for (int i=0; i<cnt; i++)
+			stmt.executeUpdate("INSERT INTO project (id, name, description," +
+		    " startdate, enddate, clientname)" +
+		    " VALUES ( "+ (i+1) +" , 'testproject"+(i+1)+"' , 'testdescription"+(i+1)+"' ,  to_date('2006-10-0"+(i+1)+"','YYYY-MM-DD'), " +
+		    " to_date('2006-11-0"+(i+1)+"','YYYY-MM-DD') , 'testclient"+(i+1)+"' )");
+			stmt.close();
+		} catch (Exception ex) {
+		}
+	}
+	
 	
 
-	
+		
 }
 
