31a7a15a2c603c5cefd7afbc05e51f67b7ac3470
Dmitrijs.Sadovskis <Dmitrijs.Sadovskis@3f29d60b-5d35-0410-bcc3-1970b16363d6>
2007-07-24 09:11:36 +0000

    add: setStatus() to get status also

--- trunk/src/java/com/jds/architecture/service/dao/assembler/SkillsAssembler.java
+++ trunk/src/java/com/jds/architecture/service/dao/assembler/SkillsAssembler.java
@@ -1,4 +1,3 @@
-
 package com.jds.architecture.service.dao.assembler;
 
 import java.sql.PreparedStatement;
@@ -11,27 +10,29 @@ import com.jds.apps.beans.SkillsInformation;
 import com.jds.architecture.service.dao.DAOConstants;
 
 /**
- * SkillsAssembler is a class used for populating prepared statement
- * and for retrieving skill from result set.
+ * SkillsAssembler is a class used for populating prepared statement and for
+ * retrieving skill from result set.
  * 
  * @author Ivars Lacis
  */
-public class SkillsAssembler
-{
+public class SkillsAssembler {
 	/**
 	 * Populates prepared statement using details of value object
 	 * 
-	 * @param employee skill value object
-	 * @param stmt prepared statement from data access object
-	 * @throws SQLException 
+	 * @param employee
+	 *            skill value object
+	 * @param stmt
+	 *            prepared statement from data access object
+	 * @throws SQLException
 	 */
-	public static void  getPreparedStatement(SkillsInformation skill, PreparedStatement stmt) throws SQLException
-	{
+	public static void getPreparedStatement(SkillsInformation skill,
+			PreparedStatement stmt) throws SQLException {
 		stmt.setString(1, skill.getSkillId());
 		stmt.setString(2, skill.getCategoryId());
 		stmt.setString(3, skill.getSkillName());
 		stmt.setString(4, skill.getSkillDescription());
 	}
+
 	/**
 	 * Creates populated SkillSInformation from the Row set
 	 * 
@@ -39,17 +40,18 @@ public class SkillsAssembler
 	 * @return EmployeeInfo
 	 * @throws SQLException
 	 */
-	public static SkillsInformation getInfo(ResultSet rs) throws SQLException
-	{
-		SkillsInformation skillReturn  = new SkillsInformation();
-		
+	public static SkillsInformation getInfo(ResultSet rs) throws SQLException {
+		SkillsInformation skillReturn = new SkillsInformation();
+
 		skillReturn.setSkillId(rs.getString("id"));
 		skillReturn.setCategoryId(rs.getString("catid"));
 		skillReturn.setSkillName(rs.getString("name"));
 		skillReturn.setSkillDescription(rs.getString("description"));
-		
+		skillReturn.setStatus(rs.getString("status"));
+
 		return skillReturn;
 	}
+
 	/**
 	 * Creates populated SkillSInformation from the Result set
 	 * 
@@ -57,25 +59,25 @@ public class SkillsAssembler
 	 * @return EmployeeInfo
 	 * @throws SQLException
 	 */
-	public static SkillsInformation getInfo(RowSet rs) throws SQLException
-	{
-		SkillsInformation skillReturn  = new SkillsInformation();
-		
+	public static SkillsInformation getInfo(RowSet rs) throws SQLException {
+		SkillsInformation skillReturn = new SkillsInformation();
+
 		skillReturn.setSkillId(rs.getString("id"));
 		skillReturn.setCategoryId(rs.getString("categoryId"));
 		skillReturn.setSkillName(rs.getString("skill"));
 		skillReturn.setSkillDescription(rs.getString("description"));
-		
+		skillReturn.setStatus(rs.getString("status"));
+
 		return skillReturn;
 	}
+
 	/**
 	 * 
 	 * 
 	 * @param SkillsInformation
 	 */
-	public static void toEmptyStringAllNull(SkillsInformation obj)
-	{
-		if(obj.getSkillDescription() == null)
+	public static void toEmptyStringAllNull(SkillsInformation obj) {
+		if (obj.getSkillDescription() == null)
 			obj.setSkillDescription(DAOConstants.STR_SPACE);
 	}
 }
